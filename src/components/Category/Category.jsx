196cc450472b5af6270cce8744caeb2bb2581df4
Juli82b4 <juliya.dian@gmail.com>
2025-03-18 18:46:56 +0100

    Add Category component and integrate it into Forside page

---
+++ src/components/Category/Category.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import useCategories from "../../hooks/useCategories";
+import styles from "./category.module.css";
+
+const Category = () => {
+  const { categories, isLoading, fetchError } = useCategories();
+
+  if (isLoading) return <p>Loading categories...</p>;
+  if (fetchError) return <p>Error: {fetchError}</p>;
+
+  return (
+    <div className={styles.container}>
+      <h1>Categories</h1>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <div key={category.id}>
+            <div
+              className={styles.categoryItem}
+              style={{ backgroundImage: `url(${category.image})` }}
+            >
+              <h2>{category.name}</h2>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/pages/Forside/Forside.jsx
+++ src/pages/Forside/Forside.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
 import Intro from "../../components/Intro/Intro";
+import Category from "../../components/Category/Category";
 
 const Forside = () => {
   const textContent = `Hos os handler det om den perfekte pizza med den sprødeste skorpe. Vi bruger kun de bedste råvarer til både klassiske favoritter og spændende specialiteter som "Parma Drama" og "Rabbit Royale". Uanset om du er til en lille, personlig pizza eller en stor familiedeling, så finder du det hos os. Kom forbi og nyd en pizza lavet med kærlighed, eller bestil den, hent den og nyd den derhjemme!`;
@@ -9,6 +10,7 @@ const Forside = () => {
     <>
       <Hero />
       <Intro title="Velkommen til Den Glade Skorpe!" text={textContent} />
+      <Category />
     </>
   );
 };
