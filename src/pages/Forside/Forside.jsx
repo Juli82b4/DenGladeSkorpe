d36ed4dc305634e4fa9a8d268d55f35b5260ede7
Juli82b4 <juliya.dian@gmail.com>
2025-03-18 18:31:19 +0100

    Add Intro component and integrate it into Forside page

--- src/pages/Forside/Forside.jsx
+++ src/pages/Forside/Forside.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navigation from "../../components/Navigation/Navigation";
 import Hero from "../../components/Hero/Hero";
+import Intro from "../../components/Intro/Intro";
 
 const Forside = () => {
+  const textContent = `Hos os handler det om den perfekte pizza med den sprødeste skorpe. Vi bruger kun de bedste råvarer til både klassiske favoritter og spændende specialiteter som "Parma Drama" og "Rabbit Royale". Uanset om du er til en lille, personlig pizza eller en stor familiedeling, så finder du det hos os. Kom forbi og nyd en pizza lavet med kærlighed, eller bestil den, hent den og nyd den derhjemme!`;
   return (
     <>
       <Hero />
+      <Intro title="Velkommen til Den Glade Skorpe!" text={textContent} />
     </>
   );
 };
