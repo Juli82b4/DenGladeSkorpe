d2f5b4fe50fb6975e0b65771425459fd7d6e3f2f
Juli82b4 <juliya.dian@gmail.com>
2025-03-19 13:29:36 +0100

    Remove unnecessary className from Intro component in Forside page

--- src/pages/Forside/Forside.jsx
+++ src/pages/Forside/Forside.jsx
@@ -6,14 +6,12 @@ import Category from "../../components/Category/Category";
 import Dishes from "../../components/Dishes/Dishes";
 import Footer from "../../components/Footer/Footer";
 
-
 const Forside = () => {
   const textContent = `Hos os handler det om den perfekte pizza med den sprødeste skorpe. Vi bruger kun de bedste råvarer til både klassiske favoritter og spændende specialiteter som "Parma Drama" og "Rabbit Royale". Uanset om du er til en lille, personlig pizza eller en stor familiedeling, så finder du det hos os. Kom forbi og nyd en pizza lavet med kærlighed, eller bestil den, hent den og nyd den derhjemme!`;
   return (
     <>
       <Hero />
-      <Intro title="Velkommen til Den Glade Skorpe!" text={textContent} className={styles.titleIntro}
-       />
+      <Intro title="Velkommen til Den Glade Skorpe!" text={textContent} />
       <Category />
       <Dishes />
       <Footer />
