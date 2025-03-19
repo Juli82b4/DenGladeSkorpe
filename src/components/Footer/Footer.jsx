24153df87dae7c80038ee05ae79d453d7ad1f845
Juli82b4 <juliya.dian@gmail.com>
2025-03-19 08:44:01 +0100

    Add Footer component with contact information and integrate it into Forside page

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import styles from "./footer.module.css";
+import Logo from "../Logo/Logo"
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className={styles.footer}>
+      <Logo />
+      <div className={styles.info}>
+        <p>Email: gladeskropepizza.dk</p>
+        <p>Tlf: 12345678</p>
+        <p>Address: Skropeej 42, 1234 Pizzagen</p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/pages/Forside/Forside.jsx
+++ src/pages/Forside/Forside.jsx
@@ -4,6 +4,7 @@ import Hero from "../../components/Hero/Hero";
 import Intro from "../../components/Intro/Intro";
 import Category from "../../components/Category/Category";
 import Dishes from "../../components/Dishes/Dishes";
+import Footer from "../../components/Footer/Footer";
 
 const Forside = () => {
   const textContent = `Hos os handler det om den perfekte pizza med den sprødeste skorpe. Vi bruger kun de bedste råvarer til både klassiske favoritter og spændende specialiteter som "Parma Drama" og "Rabbit Royale". Uanset om du er til en lille, personlig pizza eller en stor familiedeling, så finder du det hos os. Kom forbi og nyd en pizza lavet med kærlighed, eller bestil den, hent den og nyd den derhjemme!`;
@@ -13,6 +14,7 @@ const Forside = () => {
       <Intro title="Velkommen til Den Glade Skorpe!" text={textContent} />
       <Category />
       <Dishes />
+      <Footer />
     </>
   );
 };
