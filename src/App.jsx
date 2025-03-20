c9795e0fc94cbfa7dd29247ce1e0484bc995721b
Juli82b4 <juliya.dian@gmail.com>
2025-03-20 13:59:36 +0100

    Implement authentication context and protected routes for login functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from "react";
 import "./App.css";
-import {
-  useRoutes,
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useRoutes } from "react-router-dom";
 import Forside from "./pages/Forside/Forside";
 import Personalt from "./pages/Personalt/Personalt";
 import Kontakt from "./pages/Kontakt/Kontakt";
 import Kurv from "./pages/Kurv/Kurv";
 import Tak from "./components/Tak/Tak";
 import DishDetailPage from "./pages/Detail/DishDetailPage";
+import ProtectedRoute from "./components/login/ProtectedRoute";
+import Backoffice from "./components/Backoffice/BackofficeSection";
+import Login from "./components/login/Login";
+import { useAuthContext } from "./context/useAuthContext";
 
 function App() {
+  const { signedIn } = useAuthContext();
   const routes = useRoutes([
     { path: "/", element: <Forside /> },
     { path: "/forside", element: <Forside /> },
@@ -22,9 +22,47 @@ function App() {
     { path: "/dish/:id", element: <DishDetailPage /> },
     { path: "/kurv", element: <Kurv /> },
     { path: "/tak", element: <Tak /> },
+    { path: "/login", element: <Login /> },
+
+    {
+      path: "/backoffice",
+      element: (
+        <ProtectedRoute isAllowed={signedIn}>
+          <Backoffice />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "reviews",
+          element: <div>reviews</div>,
+        },
+        {
+          path: "stays",
+          element: <div>stays</div>,
+        },
+        {
+          path: "activities",
+          element: <div>activities</div>,
+          children: [
+            {
+              path: "add",
+              element: <div>add</div>,
+            },
+            {
+              path: "edit/:id",
+              element: <div>edit/:id</div>,
+            },
+          ],
+        },
+      ],
+    },
   ]);
 
-  return routes;
+  return (
+    <div className="app">
+      <div className="main">{routes}</div>
+    </div>
+  );
 }
 
 export default App;
